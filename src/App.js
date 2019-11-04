8e7e3b0ec70f9fd677aa15fe626f34fb2543e29f
Andrea Bender <andreab.1726@gmail.com>
2019-11-04 14:23:29 -0700

    added smoke and snapshot test

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+import Messages from './Messages';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>YOUR APPLICATION NAME!</h1>
+        <Messages name="Messages" unread={0}/>
+        <Messages name="Notifications" unread={10}/>
+      </div>
+    );
+  }
 }
 
 export default App;
